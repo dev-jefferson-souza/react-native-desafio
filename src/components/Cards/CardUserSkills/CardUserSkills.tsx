343fffb6289b0a189acee51f587f4231c4c8146f
Jefferson Souza <jefferson.souza.correa@gmail.com>
2023-03-15 23:29:51 -0300

    style(CardUserSkill): adaptando card para descrições longas

--- src/components/Cards/CardUserSkills/CardUserSkills.tsx
+++ src/components/Cards/CardUserSkills/CardUserSkills.tsx
@@ -1,130 +1,152 @@
-import React from "react"
-import AsyncStorage from "@react-native-async-storage/async-storage"
-import { Alert, Text, Image, TouchableOpacity, TouchableOpacityProps, View } from "react-native"
-import { AntDesign } from '@expo/vector-icons'; 
-import userSkillservice from "../../../api/services/userSkillService"
-import { userSkillModelUPDATE } from "../../../models/userSkillModel"
+import { AntDesign } from "@expo/vector-icons";
+import React from "react";
+import {
+  Alert,
+  Image,
+  Text,
+  TouchableOpacity,
+  TouchableOpacityProps,
+  View,
+} from "react-native";
+import userSkillservice from "../../../api/services/userSkillService";
 
-import { styles } from "./styles"
-import skillService from "../../../api/services/skillService"
 import { AuthContext } from "../../../context/AuthContext";
+import {
+  userSkillModel,
+  userSkillUpdateModel,
+} from "../../../models/userSkillModel";
+import { styles } from "./styles";
 
-interface CardUserSkillsProps extends TouchableOpacityProps{
-    knowledgeLevel: number,
-    id: number,
-    userSkillID: number,
-    onpress: any,
-    createdAt: string,
-    user: number
+interface CardUserSkillsProps extends TouchableOpacityProps {
+  skillInfo: userSkillModel;
 }
 
-export const CardUserSkills = ({knowledgeLevel, id, createdAt, onpress, user, userSkillID, ...rest} : CardUserSkillsProps) => {
+export const CardUserSkills = ({ skillInfo }: CardUserSkillsProps) => {
+  const { getUsersSkillsUpdated, user } = React.useContext(AuthContext);
+  const [newLevel, setNewLevel] = React.useState<number>(
+    skillInfo.knowledgeLevel
+  );
+  const [numberOfLines, setNumberOfLines] = React.useState<number>(3);
 
-    
-    const [newLevel, setNewLevel] = React.useState<number>(knowledgeLevel)
-    const { filteringSkils } = React.useContext(AuthContext)
-    const [skillInfo, setSkillInfo] = React.useState({name: "", version: "",  imageUrl: "a", description:""})
-    const [userID, setUserID] = React.useState(null);
+  function formatDate(date) {
+    const day = date.getDate().toString().padStart(2, "0");
+    const month = (date.getMonth() + 1).toString().padStart(2, "0");
+    const year = date.getFullYear().toString();
+    const hours = date.getHours().toString().padStart(2, "0");
+    const minutes = date.getMinutes().toString().padStart(2, "0");
+    const seconds = date.getSeconds().toString().padStart(2, "0");
 
+    return `${day}/${month}/${year} ${hours}:${minutes}:${seconds}`;
+  }
 
-    const verifyingUser = async () => {
-        const response = await AsyncStorage.getItem("@ID")
-        if(response != null || response != undefined){
-            setUserID(response)
-        }
-      }
+  const now = new Date();
+  const formattedDate = formatDate(now);
 
-    React.useEffect(() => {
-        handleSkill()
-        verifyingUser()
-    }, [])
+  const userSkill: userSkillUpdateModel = {
+    knowledgeLevel: newLevel,
+    updatedAt: formattedDate,
+    skill: { id: skillInfo.id },
+    createdAt: skillInfo.createdAt,
+    user: { id: user.id },
+  };
 
-    const handleSkill = async () => {
-        try{
-            const response = await skillService.skillGET(id)
-            setSkillInfo(response.data)
-        }catch(err){
-            console.log(err)
-        }
+  const updateUserSkill = async () => {
+    try {
+      const response = await userSkillservice.userSkillUPDATE(
+        skillInfo.id,
+        userSkill
+      );
+      getUsersSkillsUpdated();
+    } catch (err) {
+      console.log(err);
+      Alert.alert(
+        "Ops...",
+        "Não foi possível atualizar essa habiliidade. Tente novamente mais tarde."
+      );
     }
+  };
 
-    const getCurrentDate = () => {
-        const date = new Date().toJSON().slice(0, 10); 
-        return date
-    };
+  const removeUserSkill = async () => {
+    try {
+      const response = await userSkillservice.userSkillDELETE(skillInfo.id);
+      getUsersSkillsUpdated();
+    } catch (err) {
+      console.log(err);
+      Alert.alert(
+        "Ops...",
+        "Não foi possível remover essa habiliidade. Tente novamente mais tarde."
+      );
+    }
+  };
 
-    const userSkill : userSkillModelUPDATE = {
-        knowledgeLevel: newLevel,
-        updatedAt: getCurrentDate(),
-        skill: id,
-        createdAt: createdAt,
-        user: user
-    }
+  const increaseLevel = () => {
+    newLevel == 10 ? setNewLevel(10) : setNewLevel(newLevel + 1);
+  };
 
-    const updateUserSkill = async () => {
-        try{
-            const response =  await userSkillservice.userSkillUPDATE(userSkillID, userSkill)
-            filteringSkils()
-            console.log(response.status)
-        }catch(err){
-            console.log(err)
-            Alert.alert("Ops...", "Não foi possível atualizar essa habiliidade. Tente novamente mais tarde.")
-        }
-        onpress()
-    }
+  const decreaseLevel = () => {
+    newLevel == 0 ? setNewLevel(0) : setNewLevel(newLevel - 1);
+  };
 
-    const removeUserSkill = async () => {
-        try{
-            const response =  await userSkillservice.userSkillDELETE(userSkillID)
-            filteringSkils()
-            console.log(response.status)
-        }catch(err){
-            console.log(err)
-            Alert.alert("Ops...", "Não foi possível remover essa habiliidade. Tente novamente mais tarde.")
-        }
-        onpress()
+  function handleNumberOfLines() {
+    {
+      numberOfLines === 3 ? setNumberOfLines(100) : setNumberOfLines(3);
     }
-    
-    const increaseLevel = () => {
-        newLevel == 10 ? setNewLevel(10) : setNewLevel(newLevel + 1)
-    }
+  }
 
-    const decreaseLevel = () => {
-        newLevel == 0 ? setNewLevel(0) : setNewLevel(newLevel - 1)
-    }
-
-    if(user != userID){
-        return (
-            <View>
-                <Text style={{maxWidth:0.1}}>a</Text>
-            </View>
-        )
-    }else{
-        return(
-            <View style={styles.container}>
-                <View style={{alignItems:"center"}}>
-                <Text style={styles.description} numberOfLines={1}>{skillInfo.description}</Text>
-                    <Image style={styles.image}  source={{uri: skillInfo.imageUrl}}/>
-                    <Text style={styles.title} numberOfLines={1}>{skillInfo.name}</Text>
-                </View>
-                <Text style={styles.version} numberOfLines={1}>Versão {skillInfo.version}</Text>
-                <View style={styles.levelBox}>
-                    <TouchableOpacity onPress={() => decreaseLevel()}>
-                        <AntDesign name="minuscircleo" size={24} color="#fff" />
-                    </TouchableOpacity>
-                    <Text style={styles.counterText}>Nível de conhecimento {newLevel}</Text>
-                    <TouchableOpacity onPress={() => increaseLevel()}>
-                        <AntDesign name="pluscircleo" size={24} color="#fff" />
-                    </TouchableOpacity>
-    
-                </View>
-                <TouchableOpacity style={{...styles.deleteButton, display: newLevel != knowledgeLevel ? "none" : "flex"}} activeOpacity={0.7} onPress={() => removeUserSkill()}>
-                    <Text style={styles.deleteText}>Remover</Text>
-                </TouchableOpacity>
-                <TouchableOpacity style={{...styles.updateButton, display: newLevel != knowledgeLevel ? "flex" : "none"}} activeOpacity={0.7} onPress={() => updateUserSkill()}>
-                    <Text style={styles.deleteText}>Atualizar</Text>
-                </TouchableOpacity>
-            </View>
-        )
-    }
-}
+  return (
+    <View style={styles.container}>
+      <View style={{ alignItems: "center" }}>
+        <Text style={styles.title} numberOfLines={1}>
+          {skillInfo.skill.name}
+        </Text>
+        <Image
+          style={styles.image}
+          source={{ uri: skillInfo.skill.image_url }}
+        />
+        <Text
+          style={styles.description}
+          numberOfLines={numberOfLines}
+          onPress={handleNumberOfLines}
+        >
+          {skillInfo.skill.description}
+        </Text>
+      </View>
+      <View style={{ position: "absolute", bottom: 52 }}>
+        <Text style={styles.version} numberOfLines={1}>
+          Versão {skillInfo.skill.version}
+        </Text>
+        <View style={styles.levelBox}>
+          <TouchableOpacity onPress={() => decreaseLevel()}>
+            <AntDesign name="minuscircleo" size={28} color="#fff" />
+          </TouchableOpacity>
+          <Text style={styles.counterText}>
+            Nível de conhecimento {newLevel}
+          </Text>
+          <TouchableOpacity onPress={() => increaseLevel()}>
+            <AntDesign name="pluscircleo" size={28} color="#fff" />
+          </TouchableOpacity>
+        </View>
+      </View>
+      <TouchableOpacity
+        style={{
+          ...styles.deleteButton,
+          display: newLevel != skillInfo.knowledgeLevel ? "none" : "flex",
+        }}
+        activeOpacity={0.7}
+        onPress={() => removeUserSkill()}
+      >
+        <Text style={styles.deleteText}>Remover</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={{
+          ...styles.updateButton,
+          display: newLevel != skillInfo.knowledgeLevel ? "flex" : "none",
+        }}
+        activeOpacity={0.7}
+        onPress={() => updateUserSkill()}
+      >
+        <Text style={styles.deleteText}>Atualizar</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
--- src/components/Cards/CardUserSkills/styles.ts
+++ src/components/Cards/CardUserSkills/styles.ts
@@ -1,82 +1,82 @@
 import { StyleSheet } from "react-native";
 
 export const styles = StyleSheet.create({
-    container: {
-        height: 300,
-        width: 220,
-        backgroundColor:'#151519',
-        borderRadius: 12,
-        justifyContent:"space-between",
-        paddingTop: 18,
-        marginHorizontal: 4
-    },
-    image:{
-        width: 100,
-        height: 100,
-        borderRadius: 12,
-        backgroundColor:'#151519',
-        resizeMode:"contain",
-        flexDirection:"row",
-        marginRight: 8
-        
-    },
-    title:{
-        fontSize: 18,
-        textAlign:"center",
-        fontWeight: "800",
-        color: '#fff',
-        zIndex: 2,
-    },
-    description:{
-        fontSize: 16,
-        textAlign:"center",
-        fontWeight: "800",
-        color: '#fff',
-        zIndex: 2,
-        marginBottom: 8
-    },
-    version:{
-        fontSize: 14,
-        padding: 4,
-        height: 24,
-        fontWeight: '900',
-        color: '#fff',
-        textAlign:"center",
-        borderRadius: 12,
-    },
-    deleteButton:{
-        backgroundColor:'#de2a30',
-        borderBottomLeftRadius: 8,
-        borderBottomRightRadius: 8,
-        paddingVertical: 8,
-        width: '100%'
-    },
-    updateButton:{
-        backgroundColor:'#4611ad',
-        borderBottomLeftRadius: 8,
-        borderBottomRightRadius: 8,
-        paddingVertical: 8,
-        width: '100%'
-    },
-    deleteText:{
-        textAlign:"center",
-        fontWeight: '800',
-        color: '#fff'
-    },
-    levelBox:{
-        flexDirection:"row",
-        justifyContent:"space-evenly",
-        alignItems:"center",
-        alignSelf:"center",
-        width: '100%',
-        marginVertical: 12
-    },
-    counterText:{
-        color: '#fff',
-        width: 90,
-        textAlign:"center",
-        fontSize: 12,
-        fontWeight: "800"
-    }
-  })
-  
+  container: {
+    height: 500,
+    width: 300,
+    backgroundColor: "#151519",
+    borderRadius: 12,
+    justifyContent: "space-between",
+    paddingTop: 18,
+    marginHorizontal: 4,
+  },
+  image: {
+    width: 170,
+    height: 170,
+    borderRadius: 12,
+    backgroundColor: "#151519",
+    resizeMode: "center",
+    flexDirection: "row",
+    marginRight: 8,
+    marginBottom: 12,
+  },
+  title: {
+    fontSize: 26,
+    textAlign: "center",
+    fontWeight: "800",
+    color: "#fff",
+    zIndex: 2,
+    marginBottom: 12,
+  },
+  description: {
+    fontSize: 14,
+    fontWeight: "800",
+    color: "#fff",
+    zIndex: 2,
+    marginBottom: 8,
+    paddingHorizontal: 14,
+  },
+  version: {
+    fontSize: 14,
+    padding: 4,
+    height: 24,
+    fontWeight: "900",
+    color: "#fff",
+    textAlign: "center",
+    borderRadius: 12,
+  },
+  deleteButton: {
+    backgroundColor: "#de2a30",
+    borderBottomLeftRadius: 8,
+    borderBottomRightRadius: 8,
+    paddingVertical: 12,
+    width: "100%",
+  },
+  updateButton: {
+    backgroundColor: "#4611ad",
+    borderBottomLeftRadius: 8,
+    borderBottomRightRadius: 8,
+    paddingVertical: 12,
+    width: "100%",
+  },
+  deleteText: {
+    textAlign: "center",
+    fontWeight: "800",
+    color: "#fff",
+  },
+  levelBox: {
+    flexDirection: "row",
+    justifyContent: "space-evenly",
+    alignItems: "center",
+    alignSelf: "center",
+    width: "100%",
+    marginVertical: 12,
+  },
+  counterText: {
+    color: "#fff",
+    width: 90,
+    textAlign: "center",
+    fontSize: 12,
+    fontWeight: "800",
+  },
+});
